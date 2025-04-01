53e2b4e0a762627415a20b114b58d24578d92129
somotto <stephenonyango4783@gmail.com>
2025-04-01 19:51:05 +0300

    feat: Add looping functionality to the project

---
+++ looping/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{self, Write};
+
+fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let answer = "the letter e";
+    let mut attempts = 0;
+    
+    loop {
+        println!("{}", riddle);
+        
+        print!("> ");
+        io::stdout().flush().unwrap();
+        
+        let mut user_answer = String::new();
+        io::stdin()
+            .read_line(&mut user_answer)
+            .expect("Failed to read line");
+        
+        attempts += 1;
+        
+        let user_answer = user_answer.trim().to_lowercase();
+        
+        if user_answer == answer || user_answer == "e" {
+            println!("Number of trials: {}", attempts);
+            break;
+        }
+    }
+}
