f5b2bd3eb744fe10150ed2cd1fb20d0701501942
somotto <stephenonyango4783@gmail.com>
2025-04-01 15:43:28 +0300

    ref: change data types

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -7,21 +7,13 @@ pub fn diff(a: i16, b: i16) -> i16 {
 }
 
 pub fn pro(a: i8, b: i8) -> i16 {
-    (a as i16) * (b as i16)
+    a * b
 }
 
 pub fn quo(a: i32, b: i32) -> i32 {
-    if b != 0 {
         a / b
-    } else {
-       0
-    }
 }
 
 pub fn rem(a: i32, b: i32) -> i32 {
-    if b != 0 {
         a % b
-    } else {
-      0
-    }
 }
