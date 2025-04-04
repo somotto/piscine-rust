b207a6bf4aabf27be364bbf7703b6ae1ef5c66b9
somotto <stephenonyango4783@gmail.com>
2025-04-04 21:17:53 +0300

    Refactor card_deck module to improve code organization and readability

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,72 @@
+use rand::Rng;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        match rand::thread_rng().gen_range(1..=4) {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+        }
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid suit value"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Rank {
+    Ace,
+    Number(u8),
+    Jack,
+    Queen,
+    King,
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        match rand::thread_rng().gen_range(1..=13) {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            n => Rank::Number(n),
+        }
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            2..=10 => Rank::Number(value),
+            _ => panic!("Invalid rank value"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    matches!(card, Card { suit: Suit::Spade, rank: Rank::Ace })
+}
