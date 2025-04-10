5ff809267c46e5d06261a4b27fc0fa215d3c3362
somotto <stephenonyango4783@gmail.com>
2025-04-10 20:25:07 +0300

    remove import statements

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,18 +1,18 @@
 pub fn area_fit(
     x: usize,
     y: usize,
-    objects: areas_volumes::GeometricalShapes,
+    objects: GeometricalShapes,
     times: usize,
     a: usize,
     b: usize,
 ) -> bool {
-    let container_area = areas_volumes::rectangle_area(x, y);
+    let container_area = rectangle_area(x, y);
     
     let single_object_area = match objects {
-        areas_volumes::GeometricalShapes::Square => areas_volumes::square_area(a) as f64,
-        areas_volumes::GeometricalShapes::Circle => areas_volumes::circle_area(a),
-        areas_volumes::GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) as f64,
-        areas_volumes::GeometricalShapes::Triangle => areas_volumes::triangle_area(a, b),
+        GeometricalShapes::Square => square_area(a) as f64,
+        GeometricalShapes::Circle => circle_area(a),
+        GeometricalShapes::Rectangle => rectangle_area(a, b) as f64,
+        GeometricalShapes::Triangle => triangle_area(a, b),
     };
     
     let total_area_needed = single_object_area * times as f64;
@@ -24,23 +24,23 @@ pub fn volume_fit(
     x: usize,
     y: usize,
     z: usize,
-    objects: areas_volumes::GeometricalVolumes,
+    objects: GeometricalVolumes,
     times: usize,
     a: usize,
     b: usize,
     c: usize,
 ) -> bool {
-    let container_volume = areas_volumes::parallelepiped_volume(x, y, z);
+    let container_volume = parallelepiped_volume(x, y, z);
     
     let single_object_volume = match objects {
-        areas_volumes::GeometricalVolumes::Cube => areas_volumes::cube_volume(a) as f64,
-        areas_volumes::GeometricalVolumes::Sphere => areas_volumes::sphere_volume(a),
-        areas_volumes::GeometricalVolumes::Cone => areas_volumes::cone_volume(a, b),
-        areas_volumes::GeometricalVolumes::Pyramid => {
+        GeometricalVolumes::Cube => cube_volume(a) as f64,
+        GeometricalVolumes::Sphere => sphere_volume(a),
+        GeometricalVolumes::Cone => cone_volume(a, b),
+        GeometricalVolumes::Pyramid => {
             let base_area = a as f64; 
-            areas_volumes::triangular_pyramid_volume(base_area, b)
+            triangular_pyramid_volume(base_area, b)
         },
-        areas_volumes::GeometricalVolumes::Parallelepiped => areas_volumes::parallelepiped_volume(a, b, c) as f64,
+        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
     };
     
     let total_volume_needed = single_object_volume * times as f64;
