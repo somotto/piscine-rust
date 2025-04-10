f005a73c1e700a5c97f5a2ff2645809cd645f102
somotto <stephenonyango4783@gmail.com>
2025-04-10 19:33:04 +0300

    refactor middle day function

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,17 +1,12 @@
 use chrono::{Datelike, NaiveDate, Weekday as wd};
 
 pub fn middle_day(year: i32) -> Option<wd> {
-    let is_leap_year = chrono::NaiveDate::from_ymd_opt(year, 2, 29).is_some();
-    let days_in_year = if is_leap_year { 366 } else { 365 };
-    
-    if days_in_year % 2 == 0 {
+    let is_leap = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
+
+    if is_leap {
         return None;
     }
-    
-    let middle_day_of_year = (days_in_year + 1) / 2;
-    
-    let jan1 = NaiveDate::from_ymd_opt(year, 1, 1).unwrap();
-    let middle_date = jan1.with_ordinal(middle_day_of_year as u32).unwrap();
-    
-    Some(middle_date.weekday())
+
+    let middle = NaiveDate::from_ymd_opt(year, 1, 1)?.succ().checked_add_signed(chrono::Duration::days(181))?;
+    Some(middle.weekday())
 }
