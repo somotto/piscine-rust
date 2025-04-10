3f60201aef64a8e550a8c688ccb4ad92006e1742
somotto <stephenonyango4783@gmail.com>
2025-04-10 22:30:48 +0300

    remove import statements

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,3 +1,6 @@
+pub mod areas_volumes; 
+pub use areas_volumes::*; 
+
 pub fn area_fit(
     x: usize,
     y: usize,
@@ -6,18 +9,15 @@ pub fn area_fit(
     a: usize,
     b: usize,
 ) -> bool {
-    let container_area = rectangle_area(x, y);
-    
-    let single_object_area = match objects {
-        GeometricalShapes::Square => square_area(a) as f64,
-        GeometricalShapes::Circle => circle_area(a),
-        GeometricalShapes::Rectangle => rectangle_area(a, b) as f64,
-        GeometricalShapes::Triangle => triangle_area(a, b),
-    };
-    
-    let total_area_needed = single_object_area * times as f64;
-    
-    total_area_needed <= container_area as f64
+    let max_size = x * y;
+    let size;
+    match objects {
+        GeometricalShapes::Square => size = square_area(a) as f64, 
+        GeometricalShapes::Circle => size = circle_area(a), 
+        GeometricalShapes::Rectangle => size = rectangle_area(a, b) as f64, 
+        GeometricalShapes::Triangle => size = triangle_area(a, b), 
+    }
+    times as f64 * size <= max_size as f64 
 }
 
 pub fn volume_fit(
@@ -30,20 +30,14 @@ pub fn volume_fit(
     b: usize,
     c: usize,
 ) -> bool {
-    let container_volume = parallelepiped_volume(x, y, z);
-    
-    let single_object_volume = match objects {
-        GeometricalVolumes::Cube => cube_volume(a) as f64,
-        GeometricalVolumes::Sphere => sphere_volume(a),
-        GeometricalVolumes::Cone => cone_volume(a, b),
-        GeometricalVolumes::Pyramid => {
-            let base_area = a as f64; 
-            triangular_pyramid_volume(base_area, b)
-        },
-        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
-    };
-    
-    let total_volume_needed = single_object_volume * times as f64;
-    
-    total_volume_needed <= container_volume as f64
+    let max_size = x * y * z; 
+    let size;
+    match objects {
+        GeometricalVolumes::Cube => size = cube_volume(a) as f64, 
+        GeometricalVolumes::Sphere => size = sphere_volume(a), 
+        GeometricalVolumes::Parallelepiped => size = parallelepiped_volume(a, b, c) as f64, 
+        GeometricalVolumes::Pyramid => size = triangular_pyramid_volume(triangle_area(a, b), c), 
+        GeometricalVolumes::Cone => size = cone_volume(a, b), 
+    }
+    times as f64 * size <= max_size as f64 
 }
