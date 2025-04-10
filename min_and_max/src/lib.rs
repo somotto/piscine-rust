eac461eb1bbb5fb7e88d3a561287df6a4c1579a3
somotto <stephenonyango4783@gmail.com>
2025-04-10 05:49:58 +0300

    add min max function

---
+++ min_and_max/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn min_and_max(nb_1: i32, nb_2: i32, nb_3: i32) -> (i32, i32) {
+    let min = nb_1.min(nb_2).min(nb_3);
+    let max = nb_1.max(nb_2).max(nb_3);
+    (min, max)
+}
+
+/*
+a.min(b) returns the smaller of a and b.
+
+a.max(b) returns the larger of a and b.
+*/
