51f40de1cb66e304c734b68cb2a17092b02f059b
somotto <stephenonyango4783@gmail.com>
2025-04-11 20:16:04 +0300

    add if the word begins with a consonant

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn pig_latin(text: &str) -> String {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
+
     let text = text.to_lowercase(); 
     let chars: Vec<char> = text.chars().collect();
 
@@ -7,15 +8,17 @@ pub fn pig_latin(text: &str) -> String {
         return format!("{}ay", text);
     }
 
-    if chars.starts_with(&['q', 'u']) {
-        return format!("{}ay", text[2..].to_string() + &text[0..2]);
-    }
-
-    for i in 0..chars.len() {
-        if vowels.contains(&chars[i]) {
-            return format!("{}ay", text[i..].to_string() + &text[0..i]);
+    let mut first_vowel_pos = 0;
+    for (i, &ch) in chars.iter().enumerate() {
+        if vowels.contains(&ch) {
+            first_vowel_pos = i;
+            break;
         }
     }
 
-    format!("{}ay", text)
+    if first_vowel_pos > 1 && chars[0] == 'q' && chars[1] == 'u' {
+        return format!("{}ay", text[2..].to_string() + &text[0..2]);
+    }
+
+    format!("{}ay", text[first_vowel_pos..].to_string() + &text[0..first_vowel_pos])
 }
