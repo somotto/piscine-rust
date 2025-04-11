c8662079e3583a29319a57d976aea15d6554b6ec
somotto <stephenonyango4783@gmail.com>
2025-04-11 19:55:48 +0300

    add if the word begins with a consonant

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -3,6 +3,11 @@ pub fn pig_latin(word: &str) -> String {
     let chars: Vec<char> = word.chars().collect();
 
     match chars.iter().position(|&c| vowels.contains(&c)) {
+        Some(0) if chars.get(1) == Some(&'u') => {
+            let start = chars[2..].iter().collect::<String>();
+            let end = chars[..2].iter().collect::<String>();
+            format!("{}{}ay", start, end)
+        }
         Some(i) => {
             let start = chars[i..].iter().collect::<String>();
             let end = chars[..i].iter().collect::<String>();
