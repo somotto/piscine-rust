160002daf70f96b27634ec1853f11705b41a408f
somotto <stephenonyango4783@gmail.com>
2025-04-11 20:02:37 +0300

    add if the word begins with a consonant

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -2,17 +2,23 @@ pub fn pig_latin(word: &str) -> String {
     let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     let chars: Vec<char> = word.chars().collect();
 
-    match chars.iter().position(|&c| vowels.contains(&c)) {
-        Some(0) if chars.get(1) == Some(&'u') => {
-            let start = chars[2..].iter().collect::<String>();
-            let end = chars[..2].iter().collect::<String>();
+    match chars.first() {
+        Some(c) if vowels.contains(c) => format!("{}ay", word),
+        
+        Some('q') if chars.get(1) == Some(&'u') => {
+            let start = chars[2..].iter().collect::<String>();  
+            let end = chars[..2].iter().collect::<String>();  
             format!("{}{}ay", start, end)
+        },
+        
+        _ => {
+            if let Some(i) = chars.iter().position(|&c| vowels.contains(&c)) {
+                let start = chars[i..].iter().collect::<String>();  
+                let end = chars[..i].iter().collect::<String>();  
+                format!("{}{}ay", start, end)
+            } else {
+                String::new()  
+            }
         }
-        Some(i) => {
-            let start = chars[i..].iter().collect::<String>();
-            let end = chars[..i].iter().collect::<String>();
-            format!("{}{}ay", start, end)
-        }
-        None => String::new(),
     }
 }
