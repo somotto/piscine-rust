600bdc5f664a72c0b6f3aaf7446afff4b620d79d
somotto <stephenonyango4783@gmail.com>
2025-04-11 18:57:07 +0300

    add a function that responds according to the specified rules

---
+++ talking/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn talking(text: &str) -> &str {
+    if text.is_empty() {
+        return "Just say something!";
+    }
+
+    let is_yelling = text.chars().any(char::is_alphabetic) && 
+                     !text.chars().any(char::is_lowercase);
+    
+    let is_question = text.trim_end().ends_with('?');
+
+    match (is_yelling, is_question) {
+        (true, true) => "Quiet, I am thinking!",
+        (true, false) => "There is no need to yell, calm down!",
+        (false, true) => "Sure.",
+        _ => "Interesting",
+    }
+}
