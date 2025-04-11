2f09710b304be100bb64d83fd77006d082eab46d
somotto <stephenonyango4783@gmail.com>
2025-04-11 21:04:30 +0300

    add a function that takes a letter and returns a diamond

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -3,13 +3,15 @@ pub fn get_diamond(c: char) -> Vec<String> {
     
     let n = (c as u8 - 'A' as u8) as usize;
     
+    let width = 2 * n + 1;
+
     for i in 0..=n {
         let letter = (b'A' + i as u8) as char;
         let spaces = n - i;
-        if i == 0 {
-            diamond.push(format!("{:width$}", letter, width = n));
-        } else {
-            diamond.push(format!("{:width$}{}{:width$}", letter, " ".repeat(2 * i - 1), letter, width = spaces));
+        
+        match i {
+            0 => diamond.push(format!("{:width$}", letter)),  // First row: just 'A'
+            _ => diamond.push(format!("{:width$}{}{:width$}", letter, " ".repeat(2 * i - 1), letter, width = spaces)),  // Other rows: two letters with spaces
         }
     }
 
