5127595124769a0e28161169f3cc65683bdbf8aa
somotto <stephenonyango4783@gmail.com>
2025-04-11 16:20:51 +0300

    add a function that calculates the score of a string based on the letter values provided

---
+++ scores/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn score(input: &str) -> u64 {
+    input
+        .to_uppercase()
+        .chars()
+        .map(|c| match c {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        })
+        .sum()
+}
