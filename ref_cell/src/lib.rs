45821f1179df1eed05f3739c28d5ab67e906dabe
somotto <stephenonyango4783@gmail.com>
2025-04-15 16:16:52 +0300

    feat: implement immutability and rercell

---
+++ ref_cell/src/lib.rs
@@ -0,0 +1,42 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+mod messenger;
+pub use messenger::*;
+
+pub struct Worker {
+    pub track_value: Rc<RefCell<usize>>,
+    pub mapped_messages: RefCell<HashMap<String, String>>,
+    pub all_messages: RefCell<Vec<String>>,
+}
+
+impl Worker {
+    pub fn new(value: usize) -> Self {
+        Worker {
+            track_value: Rc::new(RefCell::new(value)),
+            mapped_messages: RefCell::new(HashMap::new()),
+            all_messages: RefCell::new(Vec::new()),
+        }
+    }
+}
+
+impl Logger for Worker {
+    fn warning(&self, msg: &str) {
+        let formatted_msg = format!("Warning: {}", msg);
+        self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(formatted_msg);
+    }
+
+    fn info(&self, msg: &str) {
+        let formatted_msg = format!("Info: {}", msg);
+        self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(formatted_msg);
+    }
+
+    fn error(&self, msg: &str) {
+        let formatted_msg = format!("Error: {}", msg);
+        self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(formatted_msg);
+    }
+}
