ab2256e5ec18984d2f0b91d6db1520caaac53ab1
somotto <stephenonyango4783@gmail.com>
2025-04-16 08:51:08 +0300

    feat: implement immutability and rercell

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -23,20 +23,20 @@ impl Worker {
 
 impl Logger for Worker {
     fn warning(&self, msg: &str) {
-        let formatted_msg = format!("Warning: {}", msg);
         self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
+        let formatted_msg = format!("Warning: {}", msg);
         self.all_messages.borrow_mut().push(formatted_msg);
     }
 
     fn info(&self, msg: &str) {
-        let formatted_msg = format!("Info: {}", msg);
         self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
+        let formatted_msg = format!("Info: {}", msg);
         self.all_messages.borrow_mut().push(formatted_msg);
     }
 
     fn error(&self, msg: &str) {
-        let formatted_msg = format!("Error: {}", msg);
         self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
+        let formatted_msg = format!("Error: {}", msg);
         self.all_messages.borrow_mut().push(formatted_msg);
     }
 }
