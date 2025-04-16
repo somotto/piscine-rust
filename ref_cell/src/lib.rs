a9013f5b2b8cb0dc6b12778226cd45d2262914a1
somotto <stephenonyango4783@gmail.com>
2025-04-16 09:06:44 +0300

    feat: implement immutability and rercell

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,19 +1,19 @@
-pub mod messenger;
+mod messenger;
 
-pub use std::cell::RefCell;
-pub use std::collections::HashMap;
-pub use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
 
-pub use messenger::{Logger, Tracker};
+use messenger::{Logger, Tracker};
 
 pub struct Worker {
-    pub track_value: Rc<i32>,
+    pub track_value: Rc<usize>,
     pub mapped_messages: RefCell<HashMap<String, String>>,
     pub all_messages: RefCell<Vec<String>>,
 }
 
 impl Worker {
-    pub fn new(initial_value: i32) -> Self {
+    pub fn new(initial_value: usize) -> Self {
         Worker {
             track_value: Rc::new(initial_value),
             mapped_messages: RefCell::new(HashMap::new()),
@@ -24,19 +24,19 @@ impl Worker {
 
 impl Logger for Worker {
     fn warning(&self, msg: &str) {
-        let full_msg = format!("{}: {}", "Warning", msg);
+        let full_msg = format!("Warning: {}", msg);
         self.all_messages.borrow_mut().push(full_msg);
         self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
     }
 
     fn info(&self, msg: &str) {
-        let full_msg = format!("{}: {}", "Info", msg);
+        let full_msg = format!("Info: {}", msg);
         self.all_messages.borrow_mut().push(full_msg);
         self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
     }
 
     fn error(&self, msg: &str) {
-        let full_msg = format!("{}: {}", "Error", msg);
+        let full_msg = format!("Error: {}", msg);
         self.all_messages.borrow_mut().push(full_msg);
         self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
     }
