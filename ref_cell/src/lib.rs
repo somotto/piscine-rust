d30661a240c6fd88e67d3e49fde5a719acf0fb08
somotto <stephenonyango4783@gmail.com>
2025-04-16 09:02:57 +0300

    feat: implement immutability and rercell

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,43 +1,43 @@
+pub mod messenger;
+
 pub use std::cell::RefCell;
 pub use std::collections::HashMap;
 pub use std::rc::Rc;
 
-mod messenger;
-pub use messenger::*;
+pub use messenger::{Logger, Tracker};
 
 pub struct Worker {
-    pub track_value: Rc<usize>,
+    pub track_value: Rc<i32>,
     pub mapped_messages: RefCell<HashMap<String, String>>,
     pub all_messages: RefCell<Vec<String>>,
 }
 
 impl Worker {
-    pub fn new(value: usize) -> Self {
-        let mut worker = Worker {
-            track_value: Rc::new(value),
+    pub fn new(initial_value: i32) -> Self {
+        Worker {
+            track_value: Rc::new(initial_value),
             mapped_messages: RefCell::new(HashMap::new()),
             all_messages: RefCell::new(Vec::new()),
-        };
-        
-        worker.mapped_messages.borrow_mut().insert("Info".to_string(), "you are using up to 40% of your quota".to_string());
-        
-        worker
+        }
     }
 }
 
 impl Logger for Worker {
     fn warning(&self, msg: &str) {
+        let full_msg = format!("{}: {}", "Warning", msg);
+        self.all_messages.borrow_mut().push(full_msg);
         self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Warning: {}", msg));
     }
 
     fn info(&self, msg: &str) {
+        let full_msg = format!("{}: {}", "Info", msg);
+        self.all_messages.borrow_mut().push(full_msg);
         self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Info: {}", msg));
     }
 
     fn error(&self, msg: &str) {
+        let full_msg = format!("{}: {}", "Error", msg);
+        self.all_messages.borrow_mut().push(full_msg);
         self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Error: {}", msg));
     }
 }
