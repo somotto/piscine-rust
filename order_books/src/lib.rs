34600a23d9902b611ae12563b06ee262da929b05
somotto <stephenonyango4783@gmail.com>
2025-04-17 07:27:26 +0300

    order books alphabeticaly

--- order_books/src/lib.rs
+++ order_books/src/lib.rs
@@ -1,4 +1,3 @@
-// Declare the library module and its submodules
 pub mod library {
     pub mod writers {
         use super::books::Book;
@@ -18,10 +17,8 @@ pub mod library {
     }
 }
 
-use library::books::Book;
 use library::writers::Writer;
 
-/// Sorts the writer's books alphabetically by title
 pub fn order_books(writer: &mut Writer) {
     writer.books.sort_by(|a, b| a.title.cmp(&b.title));
 }
