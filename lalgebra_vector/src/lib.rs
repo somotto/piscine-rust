1685d6683d9f7efdbeffc5dd591b17227c5a6fe9
somotto <stephenonyango4783@gmail.com>
2025-04-19 14:33:19 +0300

    feat: create vector trait with neccessary operations and implement it for the requested numeric types

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,61 +1,39 @@
-use std::ops::Add;
+use std::fmt::Debug;
+use std::ops::{Add, Mul};
 
-// Define the Scalar trait for types that can be used with Vector
-pub trait Scalar: Add<Output = Self> + std::fmt::Debug + Clone + PartialEq + Eq + std::ops::Mul<Output = Self> + std::marker::Sized {}
-
-// Implement Scalar for common numeric types
-impl Scalar for i32 {}
-impl Scalar for i64 {}
-impl Scalar for f32 {}
-impl Scalar for f64 {}
-
-// Vector struct that contains a Vec of elements of type T (which must implement Scalar)
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Vector<T: Scalar>(pub Vec<T>);
 
-// Implementation of Add for Vector
+pub trait Scalar: Copy + Add<Output = Self> + Mul<Output = Self> + Debug + Clone + PartialEq + Eq {}
+impl<T> Scalar for T where T: Copy + Add<Output = Self> + Mul<Output = Self> + Debug + Clone + PartialEq + Eq {}
+
 impl<T: Scalar> Add for Vector<T> {
-    type Output = Option<Self>;
+	type Output = Option<Self>;
 
-    fn add(self, other: Self) -> Self::Output {
-        // Check if vectors have the same length
-        if self.0.len() != other.0.len() {
-            return None;
-        }
-
-        // Add corresponding elements
-        let result = self.0.iter()
-            .zip(other.0.iter())
-            .map(|(a, b)| a.clone() + b.clone())
-            .collect();
-
-        Some(Vector(result))
-    }
+	fn add(self, rhs: Self) -> Self::Output {
+		if self.0.len() != rhs.0.len() {
+			return None;
+		}
+		let result = self.0.iter().zip(rhs.0.iter())
+			.map(|(a, b)| *a + *b)
+			.collect();
+		Some(Vector(result))
+	}
 }
 
 impl<T: Scalar> Vector<T> {
-    pub fn new() -> Self {
-        // Create a new empty vector
-        Vector(Vec::new())
-    }
+	pub fn new() -> Self {
+		Vector(Vec::new())
+	}
 
-    pub fn dot(&self, other: &Self) -> Option<T> {
-        // Check if vectors have the same length
-        if self.0.len() != other.0.len() {
-            return None;
-        }
-        
-        // Calculate dot product: sum of element-wise products
-        let result = self.0.iter()
-            .zip(other.0.iter())
-            .map(|(a, b)| a.clone() * b.clone())
-            .fold(None, |acc, item| {
-                match acc {
-                    None => Some(item),
-                    Some(val) => Some(val + item),
-                }
-            });
-            
-        result
-    }
+	pub fn dot(&self, other: &Self) -> Option<T> {
+		if self.0.len() != other.0.len() {
+			return None;
+		}
+		let mut result = self.0[0] * other.0[0];
+		for i in 1..self.0.len() {
+			result = result + self.0[i] * other.0[i];
+		}
+		Some(result)
+	}
 }
