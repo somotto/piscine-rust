69e9d5e2e3339ac700776624d61e7c4578e52fa3
somotto <stephenonyango4783@gmail.com>
2025-04-26 06:30:15 +0300

    feat: Add function to generate the first fifty even squares

---
+++ closures/src/lib.rs
@@ -0,0 +1,7 @@
+fn first_fifty_even_square() -> Vec<i32> {
+    (0..100)
+        .filter(|x| x % 2 == 0) 
+        .map(|x| x * x)         
+        .take(50)             
+        .collect()   
+}
