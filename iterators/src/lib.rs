9bd34ac804382081c6137378d79bc52874aadf9c
somotto <stephenonyango4783@gmail.com>
2025-04-26 20:21:04 +0300

    refactor: Improve Collatz iterator implementation

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -7,15 +7,18 @@ impl Iterator for Collatz {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.v == 0 || self.v == 1 {
+        if self.v == 0 {
             None
         } else {
-            if self.v % 2 == 0 {
+            let current = self.v;
+            if self.v == 1 {
+                self.v = 0; // Mark as finished
+            } else if self.v % 2 == 0 {
                 self.v /= 2;
             } else {
                 self.v = self.v * 3 + 1;
             }
-            Some(self.v)
+            Some(current)
         }
     }
 }
@@ -30,5 +33,5 @@ pub fn collatz(n: u64) -> usize {
     if n == 0 {
         return 0;
     }
-    Collatz::new(n).take_while(|&x| x != 1).count() + 1
+    Collatz::new(n).count() - 1
 }
