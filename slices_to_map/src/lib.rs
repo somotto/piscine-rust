42f26d476a8045ef54e5d343f3ecbb00448ad1c2
somotto <stephenonyango4783@gmail.com>
2025-04-26 21:32:42 +0300

    refactor: Simplify slices_to_map function implementation

--- slices_to_map/src/lib.rs
+++ slices_to_map/src/lib.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
-pub fn slices_to_map<T, U>(keys: &[T], values: &[U]) -> HashMap<&T, &U> {
+pub fn slices_to_map<'a, T: Eq + Hash, U>(keys: &'a [T], values: &'a [U]) -> HashMap<&'a T, &'a U> {
     let mut map = HashMap::new();
     let size = keys.len().min(values.len());
 
     for i in 0..size {
         map.insert(&keys[i], &values[i]);
     }
+
     map
 }
