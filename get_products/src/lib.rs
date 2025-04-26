2f041c4a58afced1c9bc7bbbfe2d601b1d25be6b
somotto <stephenonyango4783@gmail.com>
2025-04-26 20:07:33 +0300

    feat: Add get_products function for calculating the product of an array and generating a new array with the division of the total product by each element

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,4 +1,8 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() <= 1 {
+        return Vec::new();
+    }
+
     let total_product: usize = arr.iter().product();
     arr.iter().map(|&x| total_product / x).collect()
 }
