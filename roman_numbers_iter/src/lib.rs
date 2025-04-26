4d56e4c2be34c93ec6549862bce609c42001f509
somotto <stephenonyango4783@gmail.com>
2025-04-26 21:00:40 +0300

    refactor: Simplify next method implementation in RomanNumber iterator

--- roman_numbers_iter/src/lib.rs
+++ roman_numbers_iter/src/lib.rs
@@ -4,9 +4,8 @@ impl Iterator for RomanNumber {
     type Item = RomanNumber;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let value = self.to_u32();
-        let next_value = value + 1;
-        let next_roman = RomanNumber::from(next_value);
-        Some(next_roman)
+        let value = self.to_u32() + 1;
+        *self = RomanNumber::from(value);
+        Some(self.clone())
     }
 }
