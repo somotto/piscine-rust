44d8907957dd7c5db90c26790d30d59f922d7581
somotto <stephenonyango4783@gmail.com>
2025-04-26 21:40:31 +0300

    feat: add StepIterator implementation

---
+++ step_iterator/src/lib.rs
@@ -0,0 +1,70 @@
+pub struct StepIterator<T> {
+    current: T,
+    end: T,
+    step: T,
+    done: bool,
+}
+
+use std::ops::{Add, PartialOrd, Copy};
+
+impl<T> StepIterator<T> 
+where
+    T: Add<Output = T> + PartialOrd + Copy,
+{
+    pub fn new(beg: T, end: T, step: T) -> Self {
+        StepIterator {
+            current: beg,
+            end,
+            step,
+            done: false,
+        }
+    }
+}
+
+impl<T> Iterator for StepIterator<T> 
+where
+    T: Add<Output = T> + PartialOrd + Copy,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        let result = self.current;
+
+        self.current = self.current + self.step;
+        if (self.step > T::zero() && result > self.end) || (self.step < T::zero() && result < self.end) {
+            self.done = true;
+            return None;
+        }
+        if (self.step > T::zero() && self.current > self.end) || (self.step < T::zero() && self.current < self.end) {
+            self.done = true;
+        }
+
+        Some(result)
+    }
+}
+
+trait Zero {
+    fn zero() -> Self;
+}
+
+impl Zero for i8 { fn zero() -> Self { 0 } }
+impl Zero for i16 { fn zero() -> Self { 0 } }
+impl Zero for i32 { fn zero() -> Self { 0 } }
+impl Zero for i64 { fn zero() -> Self { 0 } }
+impl Zero for isize { fn zero() -> Self { 0 } }
+impl Zero for u8 { fn zero() -> Self { 0 } }
+impl Zero for u16 { fn zero() -> Self { 0 } }
+impl Zero for u32 { fn zero() -> Self { 0 } }
+impl Zero for u64 { fn zero() -> Self { 0 } }
+impl Zero for usize { fn zero() -> Self { 0 } }
+impl Zero for f32 { fn zero() -> Self { 0.0 } }
+impl Zero for f64 { fn zero() -> Self { 0.0 } }
+
+impl<T> StepIterator<T> 
+where
+    T: Add<Output = T> + PartialOrd + Copy + Zero,
+{}
