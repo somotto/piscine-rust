5c46a9fde8f2ecfdca09bedc29522ff6c65ba71c
somotto <stephenonyango4783@gmail.com>
2025-04-28 03:22:07 +0300

    refactor: Refactor rpn function to improve error handling and output

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -1,64 +1,81 @@
-fn rpn(expr: &str) -> Result<i64, &'static str> {
-    let tokens: Vec<&str> = expr.split_whitespace().collect();
+use std::env;
+
+fn rpn(expr: &str) {
     let mut stack: Vec<i64> = Vec::new();
 
-    for token in tokens {
+    for token in expr.split_whitespace() {
         match token {
-            "+" | "-" | "*" | "/" | "%" => {
-                if stack.len() < 2 {
-                    return Err("Error");
-                }
-                
-                let b = stack.pop().unwrap();
-                let a = stack.pop().unwrap();
-                
-                let result = match token {
-                    "+" => a + b,
-                    "-" => a - b,
-                    "*" => a * b,
-                    "/" => {
-                        if b == 0 {
-                            return Err("Error");
-                        }
-                        a / b
-                    },
-                    "%" => {
-                        if b == 0 {
-                            return Err("Error");
-                        }
-                        a % b
-                    },
-                    _ => unreachable!(),
-                };
-                
-                stack.push(result);
-            },
-            _ => {
-                match token.parse::<i64>() {
-                    Ok(num) => stack.push(num),
-                    Err(_) => return Err("Error"),
+            "+" => {
+                if let (Some(b), Some(a)) = (stack.pop(), stack.pop()) {
+                    stack.push(a + b);
+                } else {
+                    println!("Error");
+                    return;
                 }
             }
+            "-" => {
+                if let (Some(b), Some(a)) = (stack.pop(), stack.pop()) {
+                    stack.push(a - b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "*" => {
+                if let (Some(b), Some(a)) = (stack.pop(), stack.pop()) {
+                    stack.push(a * b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "/" => {
+                if let (Some(b), Some(a)) = (stack.pop(), stack.pop()) {
+                    if b == 0 {
+                        println!("Error");
+                        return;
+                    }
+                    stack.push(a / b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            "%" => {
+                if let (Some(b), Some(a)) = (stack.pop(), stack.pop()) {
+                    if b == 0 {
+                        println!("Error");
+                        return;
+                    }
+                    stack.push(a % b);
+                } else {
+                    println!("Error");
+                    return;
+                }
+            }
+            num => match num.parse::<i64>() {
+                Ok(n) => stack.push(n),
+                Err(_) => {
+                    println!("Error");
+                    return;
+                }
+            },
         }
     }
-    
-    if stack.len() != 1 {
-        return Err("Error");
+
+    if stack.len() == 1 {
+        println!("{}", stack[0]);
+    } else {
+        println!("Error");
     }
-    
-    Ok(stack[0])
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    
+    let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("Error");
         return;
     }
-    
-    match rpn(&args[1]) {
-        Ok(result) => println!("{}", result),
-        Err(err) => println!("{}", err),
-    }
+
+    rpn(&args[1]);
 }
